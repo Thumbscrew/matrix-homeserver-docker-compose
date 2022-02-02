d3957e90f764449907d54a986f9bab3d83d0969b
Thumbscrew <thumbscrw@pm.me>
2022-02-02 16:53:57 +0000

    remove nginx to allow user to build own reverse proxy
    remove volumes and use paths instead
    
    add ability to change Synapse port

--- nginx/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-envsubst '${SERVER_NAME} ${HTTPS_PORT} ${FEDERATION_HTTPS_PORT} ${SSL_CERT_PATH} ${SSL_KEY_PATH}' < /tmp/nginx/default.conf > /etc/nginx/conf.d/default.conf
-
-exec "$@"
